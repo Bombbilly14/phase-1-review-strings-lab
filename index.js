85f1bd1dda6ff4d65b965efc08babdc8e1f5b625
William German <william.j.german2@gmail.com>
2022-10-31 10:24:21 -0700

    commit tree ddc6397a47d50fe78d70bdc943e50ae594a24ca7

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser= "Yorgle"
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
